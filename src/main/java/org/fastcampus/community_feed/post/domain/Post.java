5d30dc5a6a165de3a34d062d69b3dd034b526b8e
song eojin <feshed528@gmail.com>
2025-05-23 22:21:39 +0900

    docs : 에러 메세지 한글로 변경

--- src/main/java/org/fastcampus/community_feed/post/domain/Post.java
+++ src/main/java/org/fastcampus/community_feed/post/domain/Post.java
@@ -13,10 +13,10 @@ public class Post {
 
   public Post(Long id, User author, Content content, PostPublicationState state, LikeCounter likeCounter) {
     if (author == null) {
-      throw new IllegalArgumentException("author should not be null");
+      throw new IllegalArgumentException("작성자는 존재하지 않을 수 없습니다.");
     }
     if (content == null) {
-      throw new IllegalArgumentException("content should not be null or empty");
+      throw new IllegalArgumentException("내용이 비었거나 존재하지 않을 수 없습니다.");
     }
 
     this.id = id;
@@ -32,14 +32,14 @@ public class Post {
 
   public void updateContent(User user, String content) {
     if (!author.equals(user)) {
-      throw new IllegalArgumentException("only author can update content");
+      throw new IllegalArgumentException("오직 작성자만 내용을 수정할 수 있습니다.");
     }
     this.content.updateContent(content);
   }
 
   public void like(User user) {
     if (author.equals(user)) {
-      throw new IllegalArgumentException("author cannot like own post");
+      throw new IllegalArgumentException("작성자는 자신의 게시글에 좋아요를 누를 수 없습니다.");
     }
     likeCounter.increase();
   }
